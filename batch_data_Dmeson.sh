be205d9e4e3bfb8b628f3bf766f29ee357f7d91b
Ivan V. Ovtin <ovtin@kedr-24.inp.nsk.su>
2020-04-02 17:44:54 +0700

    02042020

--- batch_data_Dmeson.sh
+++ batch_data_Dmeson.sh
@@ -36,13 +36,27 @@
 i=${SGE_TASK_ID}
 myrand=$[1000+$i]
 
-inruns="/home/ovtin/development/Dmeson/runsDmeson/runDmeson"$i
-outfile="/spool/users/ovtin/psi3770_to_D0meson_"$i".root"
 #inruns=23942
 #outfile="psi3770_to_D0meson_test.root"
 
+inruns="/home/ovtin/development/Dmeson/runsDmeson/runDmeson"$i
+outfile="/spool/users/ovtin/psi3770_to_D0meson_"$i".root"
+mintracks=4
+maxtracks=4
+minbeamtracks=2
+minIPtracks=2
+maxIPtracks=4
+minPt=100
+maxPt=2000
+minClusterEnergy=15
+minTotalEnergy=45
+minClusters=2
+maxClusters=8
+minClustersLKr=0
+minClustersCsI=0
+
 ##$HOME/development/Dmeson/analysis_Dmeson -n 200000 -o $outfile $inruns
-$HOME/development/Dmeson/analysis_Dmeson -o $outfile $inruns
+$HOME/development/Dmeson/analysis_Dmeson -a $mintracks -d $maxtracks -b $minbeamtracks -p $minIPtracks -h $maxIPtracks -s $minPt -j $maxPt -t $minClusterEnergy -e $minTotalEnergy -c $minClusters -l $maxCluster -k $minClustersLKr -i $minClustersCsI -o $outfile $inruns
 
 status=$?
 if [ $status != 0 ]; then
