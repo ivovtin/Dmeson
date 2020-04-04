bd46dae91719716336f7534e949cad807362ccf0
Ivan V. Ovtin <ovtin@kedr-24.inp.nsk.su>
2020-04-04 18:57:33 +0700

    04042020

---
+++ runsDmeson/reduce_list.C
@@ -0,0 +1,39 @@
+void reduce_list()
+{
+    string line;
+    int i=0;
+    int N=1;
+     
+    ifstream in("runs_list_Psi3770_signal_25112019.dat"); 
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            i++; 
+            cout << line << endl;
+            if(i<=2)
+            {
+              cout << i << endl;
+	      ofstream out(TString::Format("runDmeson%d",N).Data(), ios::app);
+	      if (out.is_open())
+              {
+                out << line << endl;
+              }
+              out.close();      
+            }          
+            else{
+	      ofstream out(TString::Format("runDmeson%d",N).Data(), ios::app);
+	      if (out.is_open())
+              {
+                out << line << endl;
+              }
+              out.close();      
+              i=0;
+              N++;
+            }
+        }
+    }
+    in.close();     
+     
+    cout << "End of program" << endl;
+}
