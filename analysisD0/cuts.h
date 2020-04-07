770880510e3c10ac96b02d783b0f1ca28f6b2df2
Ivan V. Ovtin <ovtin@kedr-24.inp.nsk.su>
2020-04-07 22:11:55 +0700

    07042020

---
+++ analysisD0/cuts.h
@@ -0,0 +1,14 @@
+#ifndef cuts_h
+# define cuts_h
+
+int ntrk=4;
+int nip=4;
+int nbeam=4;
+int min_trk_ncls=1;
+int min_tot_ncls=4;
+int max_tot_ncls=8;
+int maxmunhits=4;
+int mintofnhits=0;
+double minenontrk=15.;     //minimal energy on track
+
+#endif
---
+++ analysisD0/runD0meson.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+atcrange=1
+hitAer=1
+hitWLS=0
+firstCNT=0
+endCNT=160
+maxMom=5000
+dataORmc=0
+
+./D0meson $atcrange $hitAer $hitWLS $firstCNT $endCNT $maxMom $dataORmc
+
