7dd81cc9b3eccccb8ea95327ea88be2bcff1ca21
Ivan V. Ovtin <ovtin@kedr-24.inp.nsk.su>
2020-10-09 11:23:37 +0700

    lumi

--- beam_en/lumi.cpp
+++ beam_en/lumi.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <string>
 #include <list>
-#include <algorithm>    // std::min_element, std::max_element
+#include <algorithm>
 #include <math.h>
 #include <cfortran.h>
 
@@ -44,12 +44,14 @@ int main(int argc, char** argv) {
     double en=0, en_err=0, lum=0;
     int t;
 
+    int nbads=0;
+
     string line;
     //int line;
     int i=0;
+    //ifstream in("/home/ovtin/development/Dmeson/runsDmeson/runs2004/allrunsDmeson2005"); //signal
     ifstream in("/home/ovtin/development/Dmeson/runsDmeson/runs_list_Psi3770_Psi2_signal_20042020.dat"); //signal
     //ifstream in("/home/ovtin/development/Dmeson/runsDmeson/runs_list_Psi3770_signal_25112019.dat"); //signal
-    //ifstream in("/home/ovtin/development/Dmeson/runsDmeson/runsIgnoreList.txt"); //signal
     if (in.is_open())
     {
 	while (getline(in, line))
@@ -57,54 +59,22 @@ int main(int argc, char** argv) {
 	    i++;
 	    run = atoi(line.c_str());
 
-	    //==============================================
-
-	    //list bad runs for 2016-17
-	    if (
-		run==23209
-		|| run==23211
-		|| run==23911
-		|| run==23700
-		|| run==23699
-		|| run==23792
-		|| run==23745
-		|| run==24850
-		|| run==24852
-		|| run==24860
-		|| run==25086
-		|| run==25172
-		|| run==25174
-		|| run==25175
-		|| run==25217
-		|| run==25233
-		|| run==25327
-		|| run==25331
-		|| run==25337
-		|| run==25338
-		|| run==25357
-		|| run==25360
-		|| run==25365
-		|| run==25368
-		|| run==25369
-		|| run==25370
-		|| run==25598
-		|| run==25658
-		|| run==25659
-		|| run==26070
-		|| run==26071
-		|| run==26099
-		|| run==26246
-	       ) continue;
-
-            //==============================================
-
 	    get_run_lumi(run, &en, &en_err, &lum, &t);
-	    printf("%i %d %f %f %f %d\n",i,run,en,en_err,lum*0.001,t);
-	    if( lum<100000 ) slum += lum*0.001;  //nb^-1
+	    //if( lum*0.001<0.50 && run>=4100 && run<=4709 ) {
+	    if( lum*0.001<1.00 && run>=23206 && run<=26248 ) {
+		//printf("%i %d %f %f %f %d\n",i,run,en,en_err,lum*0.001,t);
+		printf("%d\n",run);
+                nbads++;
+	    }
+	    //if( lum<100000 ) slum += lum*0.001;  //nb^-1
+	    //if( lum<100000 && run>=4100 && run<=4709 ) slum += lum*0.001;  //nb^-1
+            //if( lum*0.001<0.50 && run>=4100 && run<=4709 ) slum += lum*0.001;  //nb^-1
+            if( lum*0.001<1.00 && run>=23206 && run<=26248 ) slum += lum*0.001;  //nb^-1
 	    //if( lum<100000 && run>=24613 && run<=25687 ) slum += lum*0.001;  //nb^-1
 	}
     }
     in.close();
 
     printf("Total Lumi = %f\n", slum);
+    printf("Number bad runs = %i\n", nbads);
 }
