24245a1dd6a296f0d3d3adc5d9f5019cf67abd77
Ivan V. Ovtin <ovtin@kedr-24.inp.nsk.su>
2022-04-27 22:18:38 +0700

    AprilKEDR2022-2

--- analysisD0/rooFit/load_read_write_C.d
+++
@@ -1,9 +0,0 @@
-
-# DO NOT DELETE
-
-./load_read_write_C.so: /usr/include/root/TVirtualFitter.h
-./load_read_write_C.so: /usr/include/root/TMinuit.h
-./load_read_write_C.so: /usr/include/root/TRandom3.h
-./load_read_write_C.so: /usr/include/root/TSystem.h /usr/include/root/TTree.h
-./load_read_write_C.so: /usr/include/root/RVersion.h /usr/include/root/RConfig.h /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
-load_read_write_C__ROOTBUILDVERSION= 6.22/08
